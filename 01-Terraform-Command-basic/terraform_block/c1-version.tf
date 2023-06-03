1fbf1fa1eda5d1461d7fbbb13fde8146fb6ec4f0
Ashiq Rahman <0xashiqrahman@gmail.com>
2023-06-03 15:08:53 +0530

    terraform input - data source

---
+++ 01-Terraform-Command-basic/terraform_block/c1-version.tf
@@ -0,0 +1,14 @@
+## Terraform block
+
+terraform {
+  required_version = "~> 0.14"
+  required_providers {
+    aws = {
+      source  = "value"
+      version = "value"
+    }
+  }
+}
+
+
+## Provider Block
